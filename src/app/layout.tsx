8a5b6afeb3ff7125617d9dfb612c9e067765c23f
quimeymorando <kimi.morando@gmail.com>
2026-03-23 15:50:16 -0300

    pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
+import FacebookPixel from "@/components/analytics/FacebookPixel";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -49,6 +50,7 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${outfit.variable} antialiased`}
       >
+        <FacebookPixel />
         {children}
       </body>
     </html>
